947a4c918b113a11ae4bdbc7ff93a6a94ab72af3
Student <student@DM005.local>
2019-09-05 09:10:48 -0500

    Adding Unity Project and Test Script.

---
+++ Assets/testScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
